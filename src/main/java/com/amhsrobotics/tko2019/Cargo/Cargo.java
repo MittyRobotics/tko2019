40704110e629adf23612854633bac03e933e7832
Vidit567 <43189246+Vidit567@users.noreply.github.com>
2019-01-07 12:28:12 -0800

    Added talons
    for intake and conveyor

--- src/main/java/com/amhsrobotics/tko2019/Cargo/Cargo.java
+++ src/main/java/com/amhsrobotics/tko2019/Cargo/Cargo.java
@@ -1,14 +1,30 @@
 package com.amhsrobotics.tko2019.Cargo;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Cargo {
     DigitalInput limitSwitch;
 
+    public static WPI_TalonSRX[] intakeTalons = new WPI_TalonSRX[2];
+    public static WPI_TalonSRX[] conveyorTalons = new WPI_TalonSRX[2];
 
     public void init() {
         limitSwitch = new DigitalInput(0);
         limitSwitch.get();
+
+        intakeTalons[0] = new WPI_TalonSRX(0);
+        intakeTalons[1] = new WPI_TalonSRX(1);
+
+        conveyorTalons[0] = new WPI_TalonSRX(3);
+        conveyorTalons[1] = new WPI_TalonSRX(4);
+
+        intakeTalons[1].set(ControlMode.Follower, intakeTalons[0].getDeviceID());
+        intakeTalons[0].configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+        
+        conveyorTalons[1].set(ControlMode.Follower, conveyorTalons[0].getDeviceID());
     }
 }
