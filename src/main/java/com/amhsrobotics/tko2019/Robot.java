af50ae9bfc88067d27ac7eecc124ce00cdd74196
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-07 10:07:18 -0800

    Initial Commit

---
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -0,0 +1,29 @@
+package com.amhsrobotics.tko2019;
+
+import edu.wpi.first.wpilibj.SampleRobot;
+
+public class Robot extends SampleRobot {
+	Robot() {
+
+	}
+
+	@Override
+	protected void robotInit() {
+
+	}
+
+	@Override
+	public void operatorControl() {
+
+	}
+
+	@Override
+	public void autonomous() {
+
+	}
+
+	@Override
+	public void test() {
+
+	}
+}
