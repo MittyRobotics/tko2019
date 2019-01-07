e4cbbf8f772bdc947a6ee499ad606d367651dcf1
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-01-07 14:09:16 -0800

    Compressor

--- src/main/java/com/amhsrobotics/tko2019/Robot.java
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -1,15 +1,19 @@
 package com.amhsrobotics.tko2019;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.SampleRobot;
 
 public class Robot extends SampleRobot {
-	Robot() {
+	private final Compressor compressor;
 
+	Robot() {
+		compressor = new Compressor();
 	}
 
 	@Override
 	protected void robotInit() {
-
+		compressor.start();
+		compressor.setClosedLoopControl(true);
 	}
 
 	@Override
