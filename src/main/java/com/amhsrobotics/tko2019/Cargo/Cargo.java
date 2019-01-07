dfa235a9948c402265b62586428b02af3d15e0fe
Natasha Lal <natashalal21@mittymonarch.com>
2019-01-07 11:48:35 -0800

    Adding limit switch

--- src/main/java/com/amhsrobotics/tko2019/Cargo/Cargo.java
+++ src/main/java/com/amhsrobotics/tko2019/Cargo/Cargo.java
@@ -1,4 +1,14 @@
 package com.amhsrobotics.tko2019.Cargo;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
 public class Cargo {
+    DigitalInput limitSwitch;
+
+
+    public void init() {
+        limitSwitch = new DigitalInput(0);
+        limitSwitch.get();
+    }
 }
