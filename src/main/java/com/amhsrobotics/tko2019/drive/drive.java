5e4c0629feb7fbc78dccd26917cd583c58d0dfc5
Krish Prasad <kp.kriish@gmail.com>
2019-01-08 16:04:12 -0800

    Update drive.java
    added comments

--- src/main/java/com/amhsrobotics/tko2019/drive/drive.java
+++ src/main/java/com/amhsrobotics/tko2019/drive/drive.java
@@ -5,12 +5,12 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class drive {
-
+    // creates talons
     WPI_TalonSRX[] leftDriveTalons = new WPI_TalonSRX[2];
     WPI_TalonSRX[] rightDriveTalons = new WPI_TalonSRX[2];
-
     DoubleSolenoid gearShift;
     int x = 0;
+    //initializes talons
     public void init(){
         leftDriveTalons[0] = new WPI_TalonSRX(20);
         leftDriveTalons[1] = new WPI_TalonSRX(21);
@@ -21,13 +21,16 @@ public class drive {
         rightDriveTalons[1].set(ControlMode.Follower, rightDriveTalons[0].getDeviceID());
 
     }
+    // move left function
     public void moveLeft(double value){
         leftDriveTalons[0].set(ControlMode.PercentOutput, value);
 
     }
+    //move right function
     public void moveRight(double value){
         rightDriveTalons[0].set(ControlMode.PercentOutput, value);
     }
+    //shift gear function 
     public void shiftGear(int value){
         if (value == 0)
             gearShift.set(DoubleSolenoid.Value.kReverse);
