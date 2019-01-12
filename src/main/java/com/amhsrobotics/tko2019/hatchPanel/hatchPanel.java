05e6b92b8ecf2a0e215343974c110d48d1533e49
mkorolik <mariakorolik21@mittymonarch.com>
2019-01-11 16:01:36 -0800

    Update hatchPanel.java

--- src/main/java/com/amhsrobotics/tko2019/hatchPanel/hatchPanel.java
+++ src/main/java/com/amhsrobotics/tko2019/hatchPanel/hatchPanel.java
@@ -1,34 +1,40 @@
 package com.amhsrobotics.tko2019.hatchPanel;
 
 import com.amhsrobotics.tko2019.Robot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 public class hatchPanel {
 
-    public static DoubleSolenoid solSide;
-    public static DoubleSolenoid solForward;
-    public static DigitalInput limitSwitch;
+    static DoubleSolenoid solSide = new DoubleSolenoid(0, 1);;
+    static DoubleSolenoid solForward = new DoubleSolenoid(4, 5);
+    static DigitalInput limitSwitch = new DigitalInput(1);
+    static WPI_TalonSRX slideMotor = new WPI_TalonSRX(0);
 
-    protected void init() {
-        solSide = new DoubleSolenoid(0, 1);
-        solForward = new DoubleSolenoid(2, 3);
-        limitSwitch = new DigitalInput(1);
-    }
 
-    public static void openHatch() {
+    static void openHatch() {
         solSide.set(DoubleSolenoid.Value.kReverse);
     }
-    public static void closeHatch() {
+    static void closeHatch() {
         solSide.set(DoubleSolenoid.Value.kForward);
     }
-    public static void goHatchForward() {
+    static void goHatchForward() {
         solForward.set(DoubleSolenoid.Value.kForward);
     }
-    public static void goHatchBackward() {
+    static void goHatchBackward() {
         solForward.set(DoubleSolenoid.Value.kReverse);
     }
-    public static void limitswitch() {
+    static void sliderLeft(){
+        hatchPanelAuton.execute(-8);
+    }
+    static void sliderMiddle() {
+        hatchPanelAuton.execute(0);
+    }
+    static void sliderRight() {
+        hatchPanelAuton.execute(8);
+    }
+    static void limitswitch() {
         if (limitSwitch.get()) {
             openHatch();
         }
