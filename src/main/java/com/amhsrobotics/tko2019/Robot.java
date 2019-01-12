cd73012b248a242293807f15dfccc862aa64b160
mkorolik <mariakorolik21@mittymonarch.com>
2019-01-12 15:17:32 -0800

    Revert "Merge pull request #19 from MittyRobotics/master"
    This reverts commit 46ea24b1b8cde786a94a11caece8d25fc8d50c4b, reversing
    changes made to 75d17bbfa097f1ac880b56a445c222d667f3235a.

--- src/main/java/com/amhsrobotics/tko2019/Robot.java
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -1,50 +1,40 @@
 package com.amhsrobotics.tko2019;
 
-import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.SampleRobot;
 
-@SuppressWarnings("deprecation")
-public final class Robot extends SampleRobot {
+public class Robot extends SampleRobot {
 	public static void main(final String... args) {
 		RobotBase.startRobot(Robot::new);
 	}
 
 	private final Compressor compressor;
 
-	private Robot() {
+	Robot() {
 		compressor = new Compressor();
 	}
 
 	@Override
-	protected final void robotInit() {
-
-	}
-
-	@Override
-	public final void operatorControl() {
-		enabled();
-	}
-
-	@Override
-	public final void autonomous() {
-		enabled();
-	}
-
-	@Override
-	public final void test() {
+	protected void robotInit() {
 		CameraServer.getInstance().startAutomaticCapture(0);
 		CameraServer.getInstance().startAutomaticCapture(1);
+		compressor.start();
+		compressor.setClosedLoopControl(true);
 	}
 
 	@Override
-	protected final void disabled() {
-		compressor.stop();
+	public void operatorControl() {
 	}
 
-	private void enabled() {
-		compressor.start();
-		compressor.setClosedLoopControl(true);
+	@Override
+	public void autonomous() {
+
+	}
+
+	@Override
+	public void test() {
+
 	}
 }
