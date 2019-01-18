6ca9d6f46c928f7ac673c071b66d61854ae16ced
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-01-18 14:34:01 -0800

    Adding controls

--- src/main/java/com/amhsrobotics/tko2019/Climber/Climber.java
+++ src/main/java/com/amhsrobotics/tko2019/Climber/Climber.java
@@ -3,14 +3,17 @@ package com.amhsrobotics.tko2019.Climber;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Climber {
+	static boolean isOpen = false;
 	static DoubleSolenoid climber;
 	public static void init(){
 		climber = new DoubleSolenoid(0, 1);
 	}
 	public static void openClimber(){
 		climber.set(DoubleSolenoid.Value.kForward);
+		isOpen = false;
 	}
 	public static void closeClimber(){
 		climber.set(DoubleSolenoid.Value.kReverse);
+		isOpen = true;
 	}
 }
