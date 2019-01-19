cb683372f0a6e2f1b69c69540aa457537dd3d6a7
Ishaan Sathaye <IshaanSathaye21@mittymonarch.com>
2019-01-19 14:44:44 -0800

    Manual
    Made manual buttons for each action

--- src/main/java/com/amhsrobotics/tko2019/hatchpanel/HatchPanelControls.java
+++ src/main/java/com/amhsrobotics/tko2019/hatchpanel/HatchPanelControls.java
@@ -11,7 +11,27 @@ public class HatchPanelControls extends HatchPanel {
             manual = !manual;
         }
         if(manual){
-            
+            if (leftJoy.getTriggerPressed()){
+                openHatch();
+            }
+            else if (leftJoy.getRawButton(2)){
+                closeHatch();
+            }
+            if (leftJoy.getY() > 0.1){
+                goHatchForward();
+            }
+            else if (leftJoy.getY() < -0.1){
+                goHatchBackward();
+            }
+            if (leftJoy.getRawButton(4)){
+                slideLeft();
+            }
+            else if (leftJoy.getRawButton(5)){
+                slideRight();
+            }
+            else if (leftJoy.getRawButton(3)) {
+                slideMiddle();
+            }
         }
         else{
             if (leftJoy.getRawButton(3)){
