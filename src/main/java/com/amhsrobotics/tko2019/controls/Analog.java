6d96265050cc17ebb28c9afea8fcf45f097cf585
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-01-21 10:02:14 -0800

    Adding X & Y axis to analog enums

--- src/main/java/com/amhsrobotics/tko2019/controls/Analog.java
+++ src/main/java/com/amhsrobotics/tko2019/controls/Analog.java
@@ -2,6 +2,7 @@ package com.amhsrobotics.tko2019.controls;
 
 public enum Analog {
 	XboxLTrigger, XboxRTrigger,
-	XboxLJoystick, XboxRJoystick,
-	Joystick
+	XboxLXJoystick, XboxRXJoystick,
+	XboxLYJoystick, XboxRYJoystick,
+	JoystickX, JoystickY
 }
