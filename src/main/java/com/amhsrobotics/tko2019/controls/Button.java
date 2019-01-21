5ed1facd29d9e8ff2a13ebfbcb7fe848200323eb
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-01-21 10:20:05 -0800

    Adding ids to buttons & analog stuff

--- src/main/java/com/amhsrobotics/tko2019/controls/Button.java
+++ src/main/java/com/amhsrobotics/tko2019/controls/Button.java
@@ -1,13 +1,24 @@
 package com.amhsrobotics.tko2019.controls;
 
 public enum  Button {
-	XboxA, XboxB, XboxX, XboxY,
-	XboxLBumper, XboxRBumper,
-	XboxDUp, XboxDDown, XboxDLeft, XboxDRight,
-	XboxLJoystick, XboxRJoystick,
-	JoystickTrigger,
-	Joystick2, Joystick3, Joystick4, Joystick5,
-	Joystick6, Joystick7,
-	Joystick8, Joystick9,
-	Joystick10, Joystick11
+	XboxA(1), XboxB(2), XboxX(3), XboxY(4),
+	XboxLBumper(5), XboxRBumper(6),
+	Dpad0(0), Dpad45(45), Dpad90(90), Dpad135(135),
+	Dpad180(180), Dpad225(225), Dpad270(270), Dpad315(315),
+	XboxLJoystick(9), XboxRJoystick(10),
+	JoystickTrigger(1),
+	Joystick2(2), Joystick3(3), Joystick4(4), Joystick5(5),
+	Joystick6(6), Joystick7(7),
+	Joystick8(8), Joystick9(9),
+	Joystick10(10), Joystick11(11);
+
+	private final int rawId;
+	Button(int rawId) {
+
+		this.rawId = rawId;
+	}
+
+	public int getRawId() {
+		return rawId;
+	}
 }
