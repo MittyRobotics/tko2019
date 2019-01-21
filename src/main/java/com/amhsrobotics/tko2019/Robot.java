d05cd187989c142c6d2d8403b220f4dcceb5b2b8
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-21 13:47:47 -0800

    Revert "Merge branch 'hatch-panel' into master"
    This reverts commit f5bff16fb3eaabcd11d19566812ca8dad1b024e2, reversing
    changes made to 2197ba6f629ecab0566781886527a634e7744df6.

--- src/main/java/com/amhsrobotics/tko2019/Robot.java
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -1,11 +1,12 @@
 package com.amhsrobotics.tko2019;
 
-import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.SampleRobot;
 
-public class Robot extends SampleRobot {
+@SuppressWarnings("deprecation")
+public final class Robot extends SampleRobot {
 	public static void main(final String... args) {
 		RobotBase.startRobot(Robot::new);
 	}
@@ -13,24 +14,33 @@ public class Robot extends SampleRobot {
 	private final Compressor compressor = new Compressor();
 	
 	@Override
-	protected void robotInit() {
+	protected final void robotInit() {
+
+	}
+
+	@Override
+	public final void operatorControl() {
+		enabled();
+	}
+
+	@Override
+	public final void autonomous() {
+		enabled();
+	}
+
+	@Override
+	public final void test() {
 		CameraServer.getInstance().startAutomaticCapture(0);
 		CameraServer.getInstance().startAutomaticCapture(1);
+	}
+
+	@Override
+	protected final void disabled() {
+		compressor.stop();
+	}
+
+	private void enabled() {
 		compressor.start();
 		compressor.setClosedLoopControl(true);
-	}
-
-	@Override
-	public void operatorControl() {
-	}
-
-	@Override
-	public void autonomous() {
-
-	}
-
-	@Override
-	public void test() {
-
 	}
 }
