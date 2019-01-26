79e9f69dfb24b5587caa0dfc92f1e7d737aa7aa1
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-01-25 19:28:06 -0800

    Update Climber.java

--- src/main/java/com/amhsrobotics/tko2019/climber/Climber.java
+++ src/main/java/com/amhsrobotics/tko2019/climber/Climber.java
@@ -6,8 +6,6 @@ import com.amhsrobotics.tko2019.controls.DigitalType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class Climber {
-	private boolean isOpen = false;
-
 	private final int[] climberId = {0, 1}; //TODO
 	private DoubleSolenoid climber;
 
@@ -16,20 +14,10 @@ public class Climber {
 	}
 	public void run(){
 		Controls.getInstance().registerDigitalCommand(0, DigitalInput.XboxA, DigitalType.DigitalPress, ()->{
-			if(isOpen){
-				closeClimber();
-			}
-			else {
 				openClimber();
-			}
 		});
 	}
 	private void openClimber(){
 		climber.set(DoubleSolenoid.Value.kForward);
-		isOpen = true;
-	}
-	private void closeClimber(){
-		climber.set(DoubleSolenoid.Value.kReverse);
-		isOpen = false;
 	}
 }
