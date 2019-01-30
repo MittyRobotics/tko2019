2bf6c3c0925732e1ed19d1a99136927849bb0163
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-29 16:04:59 -0800

    Initialization

--- src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
+++ src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
@@ -1,5 +1,7 @@
 package com.amhsrobotics.tko2019.networktables;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 public class NetworkTables {
 	private static NetworkTables ourInstance = new NetworkTables();
 
@@ -7,7 +9,11 @@ public class NetworkTables {
 		return ourInstance;
 	}
 
+	private final NetworkTable primaryTable;
+
 	private NetworkTables() {
+		final NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
+		primaryTable = networkTableInstance.getTable("/SmartDashboard");
 	}
 
 }
