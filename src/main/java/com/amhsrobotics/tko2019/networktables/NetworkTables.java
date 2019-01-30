4887604d30d5de3dffc9c24e91d57760eb312e10
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-29 16:05:13 -0800

    Pre-Listening

--- src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
+++ src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
@@ -2,6 +2,8 @@ package com.amhsrobotics.tko2019.networktables;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.TableEntryListener;
+
 public class NetworkTables {
 	private static NetworkTables ourInstance = new NetworkTables();
 
@@ -16,4 +18,11 @@ public class NetworkTables {
 		primaryTable = networkTableInstance.getTable("/SmartDashboard");
 	}
 
+	void registerToTableChange(NetworkTableKey key, TableEntryListener listener, NetworkTableFlags... flags) {
+		int flagsValue = 0;
+		for (NetworkTableFlags flag: flags) {
+
+		}
+		primaryTable.addEntryListener(key.toString(), listener, flagsValue);
+	}
 }
