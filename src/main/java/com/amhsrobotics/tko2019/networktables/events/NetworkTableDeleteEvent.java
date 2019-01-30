f7ec0375e34d67370da7dca8e44c848daaea71be
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-29 16:42:39 -0800

    Added and Finished Event Generation

--- src/main/java/com/amhsrobotics/tko2019/networktables/events/NetworkTableDeleteEvent.java
+++ src/main/java/com/amhsrobotics/tko2019/networktables/events/NetworkTableDeleteEvent.java
@@ -4,8 +4,8 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableValue;
 
-public class NetworkTableUpdateEvent extends NetworkTableEvent {
-	public NetworkTableUpdateEvent(NetworkTable table, String key, NetworkTableEntry entry, NetworkTableValue value) {
+public class NetworkTableDeleteEvent extends NetworkTableEvent {
+	public NetworkTableDeleteEvent(NetworkTable table, String key, NetworkTableEntry entry, NetworkTableValue value) {
 		super(table, key, entry, value);
 	}
 }
--- src/main/java/com/amhsrobotics/tko2019/networktables/events/NetworkTableEvent.java
+++ src/main/java/com/amhsrobotics/tko2019/networktables/events/NetworkTableEvent.java
@@ -1,5 +1,19 @@
 package com.amhsrobotics.tko2019.networktables.events;
 
-public class NetworkTableEvent {
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableValue;
 
+public class NetworkTableEvent {
+	private final NetworkTable table;
+	private final String key;
+	private final NetworkTableEntry entry;
+	private final NetworkTableValue value;
+
+	public NetworkTableEvent(final NetworkTable table, final String key, final NetworkTableEntry entry, final NetworkTableValue value) {
+		this.table = table;
+		this.key = key;
+		this.entry = entry;
+		this.value = value;
+	}
 }
