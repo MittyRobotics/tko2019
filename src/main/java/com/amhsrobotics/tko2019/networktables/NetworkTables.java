2ffcc57aa70c156de8468ba426cdcccde8c8803e
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-01-29 16:04:29 -0800

    Instance

--- src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
+++ src/main/java/com/amhsrobotics/tko2019/networktables/NetworkTables.java
@@ -1,5 +1,13 @@
 package com.amhsrobotics.tko2019.networktables;
 
 public class NetworkTables {
+	private static NetworkTables ourInstance = new NetworkTables();
+
+	public static NetworkTables getInstance() {
+		return ourInstance;
+	}
+
+	private NetworkTables() {
+	}
 
 }
