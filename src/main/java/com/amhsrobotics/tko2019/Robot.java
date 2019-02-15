912f73e01f4faf18e596f957ff1978fab4a9e18d
Stella Park <westkite04@gmail.com>
2019-02-14 16:40:27 -0800

    Changes

--- src/main/java/com/amhsrobotics/tko2019/Robot.java
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -1,5 +1,13 @@
 package com.amhsrobotics.tko2019;
 
+/*
+General Test Plan:
+* Slider
+* Different pistons
+* Limit Switches (works)
+* Macro (sequences)
+ */
+
 import com.amhsrobotics.tko2019.controls.AnalogInput;
 import com.amhsrobotics.tko2019.controls.AnalogType;
 import com.amhsrobotics.tko2019.controls.Controls;
@@ -23,21 +31,29 @@ public final class Robot extends SampleRobot {
 	@Override
 	protected final void robotInit() {
 		hp.init();
-		Controls.getInstance().registerDigitalCommand(1, com.amhsrobotics.tko2019.controls.DigitalInput.Joystick3, DigitalType.DigitalPress, () -> {
-			System.out.println("Button 3");
-			hp.goHatchForward();
-		});
+//		Controls.getInstance().registerDigitalCommand(1, com.amhsrobotics.tko2019.controls.DigitalInput.Joystick3, DigitalType.DigitalPress, () -> {
+//			System.out.println("Button 3");
+//			hp.goHatchForward();
+//		});
+//
+//		Controls.getInstance().registerDigitalCommand(1, com.amhsrobotics.tko2019.controls.DigitalInput.Joystick2, DigitalType.DigitalPress, () -> {
+//			System.out.println("Button 2");
+//			hp.goHatchBackward();
+//		});
 
-		Controls.getInstance().registerDigitalCommand(1, com.amhsrobotics.tko2019.controls.DigitalInput.Joystick2, DigitalType.DigitalPress, () -> {
-			System.out.println("Button 2");
-			hp.goHatchBackward();
-		});
 
 	}
 
 	@Override
 	public final void operatorControl() {
-		Controls.getInstance().enable();
+		while (isEnabled()){
+			if(Math.abs(joystick.getY()) > 0.1){
+				hp.slideTalon.set(ControlMode.PercentOutput, joystick.getY()/5);
+			}
+			else {
+				hp.slideTalon.set(ControlMode.PercentOutput, 0);
+			}
+		}
 	}
 
 	@Override
@@ -49,9 +65,6 @@ public final class Robot extends SampleRobot {
 	public final void test() {
 		while(isEnabled()) {
 
-			//Controls.getInstance().registerAnalogCommand(1, AnalogInput.JoystickY, AnalogType.OutOfThreshold, (value) -> {
-			/////////////////////////////////////////////////////
-//				hp.slideTalon.set(ControlMode.PercentOutput, joystick.getY());
 //				System.out.println(joystick.getY());
 			/////////////////////////////////////////////////////
 			//if(value < -0.2) {
@@ -85,7 +98,6 @@ public final class Robot extends SampleRobot {
 
 			//hp.limitSwitchSafety();
 
-			//});
 
 			try {
 				Thread.sleep(20);
@@ -105,3 +117,4 @@ public final class Robot extends SampleRobot {
 		compressor.setClosedLoopControl(true);
 	}
 }
+
