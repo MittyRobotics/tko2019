11ae1c8b551d74a730952a9f96011520dfe6e9a4
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-02-18 10:25:40 -0800

    Fixing Sequences

--- src/main/java/com/amhsrobotics/tko2019/sequences/ScoreCargoCS.java
+++ src/main/java/com/amhsrobotics/tko2019/sequences/ScoreCargoCS.java
@@ -1,8 +1,58 @@
 package com.amhsrobotics.tko2019.sequences;
 
 
-import com.amhsrobotics.tko2019.sequences.States.sccsState;
+import com.amhsrobotics.tko2019.cargo.Cargo;
+import com.amhsrobotics.tko2019.drive.Drive;
+import com.amhsrobotics.tko2019.drive.PathFollower;
+import com.amhsrobotics.tko2019.sequences.States.Check;
+import com.amhsrobotics.tko2019.sequences.States.State;
+import com.amhsrobotics.tko2019.serializeddata.Coordinate;
 
 public class ScoreCargoCS {
-	sccsState state;
+    static Cargo cargo = new Cargo();
+    static Drive drive = new Drive();
+    static Sequence sequence = new Sequence();
+    static PathFollower pathFollower = new PathFollower();
+
+    public static State lastState;
+
+    public static  void Idle(){
+
+    }
+
+    public static void VisionRequest(){
+        //////////////////////
+        //Recieve Coordinate//
+        //////////////////////
+
+        Sequence.Transition(State.VISION_REQUEST, State.FOLLOWING_TARGET, new Check[]{Check.HAS_CARGO});
+    }
+
+    public static void FollowingTarget() {
+        System.out.println(Sequence.state);
+        while(sequence.completedPath == false){
+            pathFollower.FollowPath(sequence.currCoord);
+        }
+        sequence.completedPath = false;
+        Sequence.Transition(State.FOLLOWING_TARGET, State.SCORING, new Check[]{Check.HAS_CARGO, Check.RELATIVE_POS});
+    }
+
+    public static  void Scoring(){
+        System.out.println(Sequence.state);
+        double moveDist = 0; //TODO
+        cargo.cargoConveyor();
+        drive.move(moveDist);
+        cargo.intakeOuttakeMacro();
+        drive.move(-moveDist);
+        cargo.stopIntake();
+        Sequence.Transition(State.SCORING, State.EXIT_VISION,new Check[]{Check.HAS_NO_CARGO});
+    }
+
+
+    public static void ExitVision(){
+        System.out.println(Sequence.state);
+        Sequence.state = State.IDLE;
+        lastState = State.IDLE;
+
+    }
 }
--- src/main/java/com/amhsrobotics/tko2019/sequences/ScoreHatchCS.java
+++ src/main/java/com/amhsrobotics/tko2019/sequences/ScoreHatchCS.java
@@ -1,8 +1,56 @@
 package com.amhsrobotics.tko2019.sequences;
 
 
-import com.amhsrobotics.tko2019.sequences.States.shcsState;
+import com.amhsrobotics.tko2019.cargo.Cargo;
+import com.amhsrobotics.tko2019.drive.Drive;
+import com.amhsrobotics.tko2019.drive.PathFollower;
+import com.amhsrobotics.tko2019.hatchpanel.HatchPanel;
+import com.amhsrobotics.tko2019.sequences.States.Check;
+import com.amhsrobotics.tko2019.sequences.States.State;
+import com.amhsrobotics.tko2019.serializeddata.Coordinate;
 
 public class ScoreHatchCS {
-	shcsState state;
+    static HatchPanel hatch = new HatchPanel();
+    static Drive drive = new Drive();
+    static Sequence sequence = new Sequence();
+    static PathFollower pathFollower = new PathFollower();
+
+    public static State lastState;
+
+    public static  void Idle(){
+
+    }
+
+    public static void VisionRequest(){
+        //RECIEVE VISION STUFF
+
+        Sequence.Transition(State.VISION_REQUEST, State.FOLLOWING_TARGET, new Check[]{Check.HAS_HATCH});
+    }
+
+    public static void FollowingTarget() {
+        System.out.println(Sequence.state);
+        while(sequence.completedPath == false){
+            pathFollower.FollowPath(sequence.currCoord);
+        }
+        sequence.completedPath = false;
+        Sequence.Transition(State.FOLLOWING_TARGET, State.SCORING, new Check[]{Check.HAS_HATCH, Check.RELATIVE_POS});
+    }
+
+    public static  void Scoring(){
+        System.out.println(Sequence.state);
+        double moveDist = 0; //TODO
+        hatch.slideMiddle();
+        drive.move(moveDist);
+        hatch.cargoDrop();
+        drive.move(-moveDist);
+        Sequence.Transition(State.SCORING, State.EXIT_VISION,new Check[]{Check.HAS_NO_HATCH});
+    }
+
+
+    public static void ExitVision(){
+        System.out.println(Sequence.state);
+        Sequence.state = State.IDLE;
+        lastState = State.IDLE;
+
+    }
 }
