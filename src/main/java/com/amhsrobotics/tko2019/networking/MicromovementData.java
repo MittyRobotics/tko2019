2cfd1a91e31a7c9b03d1f3e2aa4c81b4d2fcb097
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-02-23 11:04:10 -0800

    Initial Base Data
    Going to be Changed

--- src/main/java/com/amhsrobotics/tko2019/networking/MicromovementData.java
+++ src/main/java/com/amhsrobotics/tko2019/networking/MicromovementData.java
@@ -3,4 +3,8 @@ package com.amhsrobotics.tko2019.networking;
 import java.io.Serializable;
 
 public class MicromovementData implements Serializable {
+    double turn1;
+    double drive1;
+    double turn2;
+    double drive2;
 }
