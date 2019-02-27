d5243f6534cf0530054f908ea456ed0bc95c7e95
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-02-26 19:22:43 -0800

    Receive Overlay

--- src/main/java/com/amhsrobotics/tko2019/networking/NetworkRequests.java
+++ src/main/java/com/amhsrobotics/tko2019/networking/NetworkRequests.java
@@ -1,10 +1,19 @@
 package com.amhsrobotics.tko2019.networking;
 
 import com.amhsrobotics.datatypes.MicromovementData;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.first.cameraserver.CameraServer;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.Arrays;
 
 public class NetworkRequests {
 	public static MicromovementData requestData(final DataPort dataPort) throws IOException, ClassNotFoundException {
@@ -12,15 +21,33 @@ public class NetworkRequests {
 		return (MicromovementData) new ObjectInputStream(socket.getInputStream()).readObject();
 	}
 
-	public static void main(String... args) {
+	public void main(String... args) {
 		try {
-			while (true) {
-				System.out.println(requestData(DataPort.Left).getTurn1());
-				System.out.println(requestData(DataPort.Center).getTurn1());
-				System.out.println(requestData(DataPort.Right).getTurn1());
-			}
+			final MicromovementData data = requestData(DataPort.Center);
+			System.out.println(Arrays.toString(data.getOverlayImage()));
+			stream(data, "t");
 		} catch (IOException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public static void stream(final MicromovementData data, final String name) throws IOException {
+		final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data.getOverlayImage());
+		final BufferedImage bufferedImage = ImageIO.read(byteArrayInputStream);
+		final Mat mat = bufferedImageToMat(bufferedImage);
+
+		CvSource outputStream = CameraServer.getInstance().putVideo(name, bufferedImage.getWidth(), bufferedImage.getHeight());
+		final Thread thread = new Thread();
+		thread.setDaemon(true);
+		while (!Thread.interrupted()) {
+			outputStream.putFrame(mat);
+		}
+	}
+
+	public static Mat bufferedImageToMat(BufferedImage bi) {
+		final Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+		byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+		mat.put(0, 0, data);
+		return mat;
 	}
 }
