0a194194056928bb597b8f065919cbf97d5dd57f
LookLotsOfPeople <LookLotsOfPeople@users.noreply.github.com>
2019-03-02 00:52:57 -0800

    Respond

--- src/main/java/com/amhsrobotics/tko2019/sequences/VisionSync.java
+++ src/main/java/com/amhsrobotics/tko2019/sequences/VisionSync.java
@@ -1,27 +1,16 @@
 package com.amhsrobotics.tko2019.sequences;
 
-import com.amhsrobotics.datatypes.MicromovementData;
 import com.amhsrobotics.tko2019.hardware.subsystems.Drive;
-import com.amhsrobotics.tko2019.networking.DataPort;
-import com.amhsrobotics.tko2019.networking.NetworkRequests;
-import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.imgcodecs.Imgcodecs;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-
-import static com.amhsrobotics.tko2019.networking.NetworkRequests.stream;
 
 public class VisionSync {
 	private static VisionSync ourInstance = new VisionSync();
-
-	private MicromovementData data = null;
 
 	public static VisionSync getInstance() {
 		return ourInstance;
@@ -39,22 +28,21 @@ public class VisionSync {
 		nt.getEntry("frame").setRaw(raw);
 	}
 
-	public static byte[] matToBytes(final Mat matrix) {
+	private static byte[] matToBytes(final Mat matrix) {
 		MatOfByte mob=new MatOfByte();
 		Imgcodecs.imencode(".jpg", matrix, mob);
 		return new ByteArrayInputStream(mob.toArray()).readAllBytes();
 	}
 
 	public void confirm() {
-		if (data != null) {
-			Drive.getInstance().turn(180- (-data.getTurn1()));
-			Drive.getInstance().moveStraight(-data.getDrive1());
-			Drive.getInstance().turn(180 - (-data.getTurn2()));
-			// Drive.getInstance().moveStraight(data.getDrive2());
-		}
-	}
-
-	public void invalidate() {
-		data = null;
+		final NetworkTableInstance nt = NetworkTableInstance.getDefault();
+		final double t1 = nt.getEntry("t1").getDouble(0);
+		final double d1 = nt.getEntry("d1").getDouble(0);
+		final double t2 = nt.getEntry("t2").getDouble(0);
+		final double d2 = nt.getEntry("d2").getDouble(0);
+		Drive.getInstance().turn(t1);
+		Drive.getInstance().moveStraight(d1);
+		Drive.getInstance().turn(t2);
+		Drive.getInstance().moveStraight(d2);
 	}
 }
