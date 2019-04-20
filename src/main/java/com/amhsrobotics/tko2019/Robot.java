baa88263dc9a1d0d606091886f2793c86104278f
Ashvin Iyer <ashviniyer21@mittymonarch.com>
2019-04-19 21:25:01 -0700

    Quick Changes

--- src/main/java/com/amhsrobotics/tko2019/Robot.java
+++ src/main/java/com/amhsrobotics/tko2019/Robot.java
@@ -3,15 +3,13 @@ package com.amhsrobotics.tko2019;
 import com.amhsrobotics.tko2019.controls.ControlBindings;
 import com.amhsrobotics.tko2019.controls.Controls;
 import com.amhsrobotics.tko2019.hardware.Compressor;
+import com.amhsrobotics.tko2019.hardware.Gyro;
 import com.amhsrobotics.tko2019.hardware.Switches;
 import com.amhsrobotics.tko2019.hardware.subsystems.Cargo;
-import com.amhsrobotics.tko2019.hardware.subsystems.Climber;
 import com.amhsrobotics.tko2019.hardware.subsystems.Drive;
 import com.amhsrobotics.tko2019.hardware.subsystems.HatchPanel;
-import com.amhsrobotics.tko2019.vision.networking.Connection;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.SampleRobot;
 
@@ -20,9 +18,6 @@ public final class Robot extends SampleRobot {
 	public static void main(final String... args) {
 		RobotBase.startRobot(Robot::new);
 	}
-//	Robot(){
-//		Connection.getInstance();
-//	}
 
 	@SuppressWarnings("ResultOfMethodCallIgnored")
 	@Override
@@ -41,16 +36,13 @@ public final class Robot extends SampleRobot {
 
 
 		// Init Hardware
-//		Gyro.getInstance();
+		Gyro.getInstance();
 		Switches.getInstance();
-
 		Compressor.getInstance();
 
 		Drive.getInstance();
 		HatchPanel.getInstance();
 		Cargo.getInstance();
-//		Connection.getInstance();
-//		Climber.getInstance();
 
 
 		// Init Controls
@@ -59,64 +51,20 @@ public final class Robot extends SampleRobot {
 
 	@Override
 	public final void autonomous() {
-//		Drive.getInstance().moveStraight(-36);
-		//Cargo 6390
 		enabled();
-//		Cargo.getInstance().moveConveyor(15734);
-//		while (isEnabled()) {
-//////			System.out.println(Drive.getInstance().leftTalons[0].getSelectedSensorPosition());
-//////			System.out.println(Drive.getInstance().leftTalons[0].getClosedLoopTarget());
-//////			try {
-//////				Thread.sleep(10);
-//////			} catch (InterruptedException e) {
-//////				e.printStackTrace();
-//////			}
-//////		}
 	}
 
 	@Override
 	public final void operatorControl() {
-//		Climber.getInstance().push();
 		enabled();
-//		while (isEnabled()){
-//			System.out.println(Drive.getInstance().leftTalons[0].getSelectedSensorPosition());
-//			System.out.println(Drive.getInstance().rightTalons[0].getSelectedSensorPosition());
-////			System.out.println("Real: " + Drive.getInstance().leftTalons[0].getMotorOutputPercent());
-//			try {
-//				Thread.sleep(10);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		VisionSync.SEQUENCES_MANAGER.continueSequence();
 	}
 
 	@Override
 	public final void test() { // Zero Encoders
 		Compressor.getInstance().start();
-//		Cargo.getInstance().moveConveyor(15000);
 		HatchPanel.getInstance().zeroEncoder();
 		Cargo.getInstance().zeroEncoder();
-		//Rockket 15730
-//		while (isEnabled()){
-//			System.out.println(Cargo.getInstance().conveyorTalons[0].getSelectedSensorPosition());
-//			try {
-//				Thread.sleep(10);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//		HatchPanel.getInstance().zeroEncoder();
-//		Drive.getInstance().moveStraight(-12);
-//		enabled();
-//		while (isEnabled()){
-//			System.out.println(Drive.getInstance().leftTalons[0].getSelectedSensorPosition());
-//			try {
-//				Thread.sleep(10);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
+
 	}
 
 	@Override
--- src/main/java/com/amhsrobotics/tko2019/hardware/Switches.java
+++ src/main/java/com/amhsrobotics/tko2019/hardware/Switches.java
@@ -8,7 +8,6 @@ public final class Switches {
 
 	private final DigitalInput intakeSensor = new DigitalInput(SwitchIds.INTAKE_SENSOR_ID);
 	private final DigitalInput hatchSwitch = new DigitalInput(SwitchIds.HATCH_SWITCH_ID);
-//	private final DigitalInput wallSwitch = new DigitalInput(SwitchIds.WALL_SWITCH_ID);
 
 	private Switches() {
 	}
@@ -25,7 +24,4 @@ public final class Switches {
 		return !hatchSwitch.get();
 	}
 
-//	public final boolean isTouchingWall() {
-//		return wallSwitch.get();
-//	}
 }
